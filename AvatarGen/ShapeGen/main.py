85fdfddf47711aa89f62bf82b13ec3578905a75d
Naor Haba <naorhabaxd@gmail.com>
2023-02-09 11:55:19 +0200

    cosmetics

--- AvatarGen/ShapeGen/main.py
+++ AvatarGen/ShapeGen/main.py
@@ -19,58 +19,61 @@ from PIL import ImageFile, Image
 
 from utils import render_one_batch, writeOBJ
 
+
 class LinearVAE(nn.Module):
     def __init__(self, in_dim, latent_dim, v_template):
         super(LinearVAE, self).__init__()
-        
+
         self.v_template = v_template
         self.latent_dim = latent_dim
 
         # encoder
         self.enc1 = nn.Linear(in_features=in_dim, out_features=8192)
-        self.enc2 = nn.Linear(in_features=8192, out_features=latent_dim*2)
- 
+        self.enc2 = nn.Linear(in_features=8192, out_features=latent_dim * 2)
+
         # decoder 
         self.dec1 = nn.Linear(in_features=latent_dim, out_features=8192)
         self.dec2 = nn.Linear(in_features=8192, out_features=in_dim)
-        
+
     def reparameterize(self, mu, log_var):
         """
         :param mu: mean from the encoder's latent space
         :param log_var: log variance from the encoder's latent space
         """
-        std = torch.exp(0.5*log_var) # standard deviation
-        eps = torch.randn_like(std) # `randn_like` as we need the same size
-        sample = mu + (eps * std) # sampling as if coming from the input space
+        std = torch.exp(0.5 * log_var)  # standard deviation
+        eps = torch.randn_like(std)  # `randn_like` as we need the same size
+        sample = mu + (eps * std)  # sampling as if coming from the input space
         return sample
- 
+
     def forward(self, x):
         latent_param = self.enc2(self.enc1(x))
         latent_param = latent_param.view(-1, 2, self.latent_dim)
-        
+
         mu = latent_param[:, 0, :]
         log_var = latent_param[:, 1, :]
-        
+
         z = self.reparameterize(mu, log_var)
- 
+
         output = self.dec2(self.dec1(z))
         return output, mu, log_var
-    
+
     def sample(self):
         zgen = torch.tensor(np.random.normal(0., 1., size=(1, self.latent_dim))).float().cuda()
         return self.dec2(self.dec1(zgen)).reshape(-1, 6890, 3) + self.v_template.reshape(1, 6890, 3), zgen
-    
+
     def sample_z(self):
         zgen = torch.tensor(np.random.normal(0., 1., size=(1, self.latent_dim))).float().cuda()
         return zgen
-    
+
     def decode(self, latent):
         return self.dec2(self.dec1(latent)).reshape(-1, 6890, 3) + self.v_template.reshape(1, 6890, 3)
+
 
 def parse_prompt(prompt):
     vals = prompt.rsplit(':', 2)
     vals = vals + ['', '1', '-inf'][len(vals):]
     return vals[0], float(vals[1]), float(vals[2])
+
 
 def create_load_AE(in_dim, latent_dim, v_template, pth_fname):
     model_AE = LinearVAE(in_dim, latent_dim, v_template)
@@ -78,10 +81,12 @@ def create_load_AE(in_dim, latent_dim, v_template, pth_fname):
     model_AE = model_AE.eval().requires_grad_(False).cuda()
     return model_AE
 
+
 def load_clip():
     perceptor, preprocess = clip.load('ViT-B/32', jit=False)
     perceptor = perceptor.eval().requires_grad_(False).cuda()
     return perceptor
+
 
 def load_codebook(fname):
     codebook_dict = torch.load(fname)
@@ -90,15 +95,16 @@ def load_codebook(fname):
         break
     return i.cuda(), clip_codebook
 
+
 def shape_gen(smpl_args, AE_path_fname, codebook_fname, neutral_txt, target_txt):
     smpl_model = build_layer(smpl_args['model_folder'], smpl_args['model_type'],
-                             gender = smpl_args['gender'], num_betas = smpl_args['num_betas']).cuda()
+                             gender=smpl_args['gender'], num_betas=smpl_args['num_betas']).cuda()
     model_AE = create_load_AE(6890 * 3, 16, smpl_model.v_template, AE_path_fname)
     perceptor = load_clip()
     codebook, clip_codebook = load_codebook(codebook_fname)
 
-    ntxt, nweight, nstop = parse_prompt(neutral_txt)
-    ttxt, tweight, tstop = parse_prompt(target_txt)
+    ntxt, nweight, nstop = parse_prompt(neutral_txt)  # nweight and nstop are not used
+    ttxt, tweight, tstop = parse_prompt(target_txt)  # nweight and nstop are not used
     nembed = perceptor.encode_text(clip.tokenize(ntxt).cuda()).float()
     tembed = perceptor.encode_text(clip.tokenize(ttxt).cuda()).float()
     delta_embed = tembed - nembed
@@ -113,14 +119,15 @@ def shape_gen(smpl_args, AE_path_fname, codebook_fname, neutral_txt, target_txt)
     images -= torch.from_numpy(np.array([0.48145466, 0.4578275, 0.40821073])).reshape(1, 3, 1, 1).cuda()
     images /= torch.from_numpy(np.array([0.26862954, 0.26130258, 0.27577711])).reshape(1, 3, 1, 1).cuda()
     neutral_image_embed = perceptor.encode_image(images).float().mean(0)
-    
-    cos_dist = (F.normalize(clip_codebook - neutral_image_embed, dim=1) * \
-            F.normalize(delta_embed.reshape(-1), dim=0)).sum(-1).reshape(-1)
+
+    # (vector / norm(vector)) * (vector / norm(vector)) = cos(vector, vector)
+    cos_dist = (F.normalize(clip_codebook - neutral_image_embed, dim=1) * F.normalize(delta_embed.reshape(-1), dim=0)).sum(-1).reshape(-1)
     best_one = cos_dist.argmax()
-    
+
     v = model_AE.decode(codebook[best_one].reshape(1, 16)).detach().cpu().numpy()
     f = smpl_model.faces
     return v.reshape(-1, 3), f, zero_beta_v.detach().cpu().numpy().reshape(-1, 3)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -140,8 +147,7 @@ if __name__ == '__main__':
     }
 
     print("Start generating coarse body shape given the target text: {}".format(args.target_txt))
-    v, f, zero_beta_v = shape_gen(smpl_args, args.AE_path_fname, args.codebook_fname, \
-                     args.neutral_txt, args.target_txt)
+    v, f, zero_beta_v = shape_gen(smpl_args, args.AE_path_fname, args.codebook_fname, args.neutral_txt, args.target_txt)
     if not os.path.exists(args.output_folder):
         os.makedirs(args.output_folder, exist_ok=True)
     output_fname = os.path.join(args.output_folder, '_'.join(args.target_txt.split(' ')) + '.obj')
--- AvatarGen/ShapeGen/render.py
+++ AvatarGen/ShapeGen/render.py
@@ -13,8 +13,10 @@ from smplx import build_layer
 
 from utils import readOBJ
 
+
 def norm_np_arr(arr):
     return arr / np.linalg.norm(arr)
+
 
 def lookat(eye, target, up):
     zaxis = norm_np_arr(eye - target)
@@ -24,10 +26,11 @@ def lookat(eye, target, up):
         [xaxis[0], xaxis[1], xaxis[2], -np.dot(xaxis, eye)],
         [yaxis[0], yaxis[1], yaxis[2], -np.dot(yaxis, eye)],
         [zaxis[0], zaxis[1], zaxis[2], -np.dot(zaxis, eye)],
-        [0       , 0       , 0       , 1                  ]
+        [0, 0, 0, 1]
     ])
     viewMatrix = np.linalg.inv(viewMatrix)
     return viewMatrix, xaxis, yaxis, zaxis
+
 
 def render_for_nerf(v, f):
     camera_distance = 2.2
@@ -36,11 +39,14 @@ def render_for_nerf(v, f):
     batch_size = v.shape[0]
     vertices = v.clone()
     faces = torch.from_numpy(f.astype(np.int32)).cuda().unsqueeze(0).repeat(batch_size, 1, 1)
-    textures = torch.ones(vertices.shape[0], faces.shape[1], texture_size, texture_size, texture_size, 3, dtype=torch.float32).cuda()
-    rot_mat = torch.from_numpy(np.array(
-        [[ 1.,  0.,  0.],
-         [ 0.,  0., -1.],
-         [ 0.,  1.,  0.]], dtype=np.float32)).cuda()
+    textures = torch.ones(vertices.shape[0], faces.shape[1], texture_size, texture_size, texture_size, 3,
+                          dtype=torch.float32).cuda()
+    rot_mat = torch.from_numpy(
+        np.array(
+            [[1., 0., 0.],
+             [0., 0., -1.],
+             [0., 1., 0.]],
+            dtype=np.float32)).cuda()
     vertices = torch.matmul(vertices, rot_mat)
     renderer = nr.Renderer(camera_mode='look_at', image_size=256).cuda()
     images_list = []
@@ -55,6 +61,7 @@ def render_for_nerf(v, f):
     images = torch.cat(images_list, 0)
     detached_images = (images.detach().cpu().numpy().transpose(0, 2, 3, 1) * 255).astype(np.uint8)
     return images, detached_images, transformation_list
+
 
 def my_lbs(v_shaped, pose, v_template, shapedirs, posedirs, J_regressor, parents, lbs_weights, pose2rot: bool = True):
     batch_size = pose.shape[0]
@@ -94,8 +101,7 @@ def my_lbs(v_shaped, pose, v_template, shapedirs, posedirs, J_regressor, parents
     W = lbs_weights.unsqueeze(dim=0).expand([batch_size, -1, -1])
     # (N x V x (J + 1)) x (N x (J + 1) x 16)
     num_joints = J_regressor.shape[0]
-    T = torch.matmul(W, A.view(batch_size, num_joints, 16)) \
-        .view(batch_size, -1, 4, 4)
+    T = torch.matmul(W, A.view(batch_size, num_joints, 16)).view(batch_size, -1, 4, 4)
 
     homogen_coord = torch.ones([batch_size, v_posed.shape[1], 1],
                                dtype=dtype, device=device)
@@ -106,9 +112,10 @@ def my_lbs(v_shaped, pose, v_template, shapedirs, posedirs, J_regressor, parents
 
     return verts, J_transformed
 
+
 def render_coarse_shape(pose, v_shaped, smpl_args, output_dir):
     smpl_model = build_layer(smpl_args['model_folder'], smpl_args['model_type'],
-                             gender = smpl_args['gender'], num_betas = smpl_args['num_betas']).cuda()
+                             gender=smpl_args['gender'], num_betas=smpl_args['num_betas']).cuda()
     pose = torch.from_numpy(pose).cuda()
     pose_rot = batch_rodrigues(pose.reshape(-1, 3)).reshape(1, 24, 3, 3)
     vertices, joints = my_lbs(
@@ -123,7 +130,7 @@ def render_coarse_shape(pose, v_shaped, smpl_args, output_dir):
     if not os.path.exists(os.path.join(output_dir, 'img')):
         os.makedirs(os.path.join(output_dir, 'img'), exist_ok=True)
     for i in range(images.shape[0]):
-        cur_img = (images[i] * 255).permute(1,2,0).type(torch.uint8).detach().cpu().numpy()
+        cur_img = (images[i] * 255).permute(1, 2, 0).type(torch.uint8).detach().cpu().numpy()
         im = Image.fromarray(cur_img)
         im.save(os.path.join(output_dir, 'img/{}.png'.format(str(i).zfill(4))))
     with open(os.path.join(output_dir, 'transforms_train.json'), 'w') as f:
@@ -138,6 +145,7 @@ def render_coarse_shape(pose, v_shaped, smpl_args, output_dir):
             })
         json.dump(data, f)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--smpl_model_folder', type=str, default='../../smpl_models')
@@ -147,9 +155,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     assert args.coarse_shape_obj is not None
-    
+
     if args.pose_type == 'stand_pose':
-        with open('./output/stand_pose.npy', 'rb') as f:
+        with open('stand_pose.npy', 'rb') as f:
             pose = np.load(f).astype(np.float32)
     elif args.pose_type == 't_pose':
         pose = np.zeros([1, 24, 3], dtype=np.float32)
