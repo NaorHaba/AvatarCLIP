04985ec7d1a397ad860408fa544bb81d2fa23b47
eitan <eitangreenb@gmail.com>
2023-02-10 21:14:00 +0200

    added last update date to render status function

--- website/utils.py
+++ website/utils.py
@@ -1,14 +1,22 @@
 import os
 from enum import Enum
-
+import time
 import streamlit as st
 
 
 def render_status(text, path):
-    icon = ":heavy_check_mark:" if os.path.exists(path) else ":x:"
-    st.markdown(f"{text}: {icon}")
+    if os.path.exists(path):
+        icon = ":heavy_check_mark:"
+        timestamp_updated = os.path.getmtime(path)
 
+        # Converting the time in seconds to a timestamp
+        date_updated = time.ctime(timestamp_updated)
+        st.markdown(f"{text}: {icon} (last updated: {date_updated})")
+    else:
+        icon = ":x:"
+        st.markdown(f"{text}: {icon}")
 
+        
 class POSE_TYPE(str, Enum):
     STAND_POSE = 'stand_pose'
     T_POSE = 't_pose'
