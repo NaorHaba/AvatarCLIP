64f8fc38d34d6edc51bbc2834380cad0f7d62af8
Naor Haba <naorhabaxd@gmail.com>
2023-02-11 19:30:36 +0200

    Update 5_Initialize_Implicit_Avatar.py

--- website/pages/5_Initialize_Implicit_Avatar.py
+++ website/pages/5_Initialize_Implicit_Avatar.py
@@ -36,7 +36,7 @@ if os.path.exists(coarse_output_folder):
             if_exists_instruction = st.radio(Messages.IF_EXISTS_INSTRUCTION, options=(Messages.CONTINUE_SELECTION, Messages.OVERWRITE_SELECTION), key="if_exists_instruction")
             choose_config = st.radio(Messages.CHOOSE_CONFIG, options=(Messages.LARGE_CONFIG, Messages.SMALL_CONFIG), key="choose_config")
             submit = st.form_submit_button(Messages.INITIALIZE_IMPLICIT_AVATAR_FORM_SUBMIT_BUTTON)
-            implicit_folder = Settings.absolute_path(os.path.join(selected_shape, Settings.IMPLICIT_AVATAR_OUTPUT_DIR))
+            implicit_folder = Settings.absolute_path(os.path.join(coarse_output_folder, selected_shape, Settings.IMPLICIT_AVATAR_OUTPUT_DIR))
             print(implicit_folder)
             if submit:
                 if choose_config == Messages.LARGE_CONFIG:
