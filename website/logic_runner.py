122954a9b9bee492514c600eb5ad1b0e89797604
eitan <eitangreenb@gmail.com>
2023-03-17 14:58:02 +0200

    fix path in logic runner

--- website/logic_runner.py
+++ website/logic_runner.py
@@ -24,14 +24,14 @@ def run_render_coarse_shape(path_to_obj_file):
 
 @request_processed_info(settings.settings['USER_EMAIL'])
 def run_initialize_implicit_avatar(implicit_config, path_to_render, is_continue):
-    run = subprocess.Popen(['python', absolute_path('logic/initialize_implicit_avatar.py'), implicit_config, path_to_render, str(is_continue), settings.settings['CURRENT_LOG_DIR']])
+    run = subprocess.Popen(['python', absolute_path('website/logic/initialize_implicit_avatar.py'), implicit_config, path_to_render, str(is_continue), settings.settings['CURRENT_LOG_DIR']])
     # save run.pid to log file
     logger.info(Messages.INITIALIZE_IMPLICIT_AVATAR_SUBPROCESS_INFO.format(run.pid))
 
 
 @request_processed_info(settings.settings['USER_EMAIL'])
 def run_generate_textures(texture_prompt, config_path, coarse_body_dir, is_continue):
-    run = subprocess.Popen(['python', absolute_path('logic/generate_textures.py'), texture_prompt, config_path, coarse_body_dir, str(is_continue), settings.settings['CURRENT_LOG_DIR']])
+    run = subprocess.Popen(['python', absolute_path('website/logic/generate_textures.py'), texture_prompt, config_path, coarse_body_dir, str(is_continue), settings.settings['CURRENT_LOG_DIR']])
     # save run.pid to log file
     logger.info(Messages.GENERATE_TEXTURES_SUBPROCESS_INFO.format(run.pid))
 
@@ -39,6 +39,6 @@ def run_generate_textures(texture_prompt, config_path, coarse_body_dir, is_conti
 @request_processed_info(settings.settings['USER_EMAIL'])
 def run_all(run_args):
     for avatar in run_args.values():
-        run = subprocess.Popen(['python', absolute_path('logic/run_all.py'), avatar['coarse_shape_prompt'], avatar['texture_description_prompt'], str(avatar['should_continue']), str(avatar['should_overwrite']), avatar['config_type'], settings.settings['CURRENT_LOG_DIR'], settings.settings['LOG_FILE_NAME']])
+        run = subprocess.Popen(['python', absolute_path('website/logic/run_all.py'), avatar['coarse_shape_prompt'], avatar['texture_description_prompt'], str(avatar['should_continue']), str(avatar['should_overwrite']), avatar['config_type'], settings.settings['CURRENT_LOG_DIR'], settings.settings['LOG_FILE_NAME']])
         # save run.pid to log file
         logger.info(Messages.RUN_ALL_SUBPROCESS_INFO.format(run.pid))
